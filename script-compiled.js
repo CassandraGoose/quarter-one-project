4cdc20a6224906ddd1dbfa804691932612cb38fc
Cassandra Torske <cassandra.torske@gmail.com>
2017-03-08 16:00:22 -0700

    thing

--- script-compiled.js
+++ script-compiled.js
@@ -38,8 +38,8 @@ $(document).ready(function () {
   });
 
   function callAPI(inputText) {
-    $.get(`https://newproxywhodis.herokuapp.com/${inputText}`, result => {
-      console.log("NEW PROXY WHO DIS");
+    $.get(`https://newproxywhodis.herokuapp.com/${ inputText }`, result => {
+
       apiInfo(result);
     });
   }
